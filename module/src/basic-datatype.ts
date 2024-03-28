841f2a1159723f10df38233c6d2a914c026ea6c7
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-28 19:59:41 +0600

    basic datatype of typescript

---
+++ module/src/basic-datatype.ts
@@ -0,0 +1,22 @@
+// basic datatype of typescript
+// string
+let bongbangname : string = "rahat"
+// num
+let roll : number = 123 
+// null
+let d : null = null;
+// undefined
+let x : undefined = undefined ;
+// boolean
+let isAdmin : boolean = true ;
+
+// Array 
+let friends : string[] = ['rahat','maruf','oli','bongbang']
+// what if i push a number on this friends 
+// friends.push(25)
+let rollList : number[] = [12,22,33]
+
+
+// TUPLE -->one kind of array but you should defined the type
+let ageName : [number,string] = [50 , 'rakib']
+let tuplehere : [number,string,boolean] = [50 , 'rakib',true];
