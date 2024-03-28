9fa9834ce6aefda2bdfee421c03dc6365c620144
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-28 20:14:01 +0600

    Reference Type

---
+++ module/src/referencetype.ts
@@ -0,0 +1,19 @@
+// Reference Type 
+
+const user : {
+  company : 'PROGRAMMER';  //optional 
+  firstName : string;
+  middleName? : string;
+  lastName : string;
+  isMarried : boolean;
+
+} =
+ {
+  company : 'PROGRAMMER',
+  firstName : 'Muhammad',
+  // middleName : 'Rahat',
+  lastName : 'Hasan',
+  isMarried : false 
+
+}
+
